e4316c20a7d8fc5d8c097b3198a631e2cb4a677c
jdavidagudelo <jdaaa2009@gmail.com>
2016-04-28 15:01:23 -0500

    Removing extra files.

--- MqttClientPublish.js
+++
@@ -1,9 +0,0 @@
-
-//var client  = mqtt.connect('mqtt://localhost', options);
-var mqtt = require("mqtt");
-var client  = mqtt.connect('mqtt://localhost', {username:'yTJGljSpPt7GNJmUc4TtQQfwwamv6I', password:""});
-client.publish("/v1.6/thg/label_ds/label_var/value/post", '{"value": 10.3}', {'qos':1, 'retain':false}, function(err, x){
-  console.log(err);
-  console.log(x);
-});
-//client.publish("/v1.6/thg/label_ds/label_var/value/lv", "10.3");
--- RedisClient.js
+++
@@ -1,5 +0,0 @@
-var redis = require("redis");
-var redisClient = redis.createClient();
-var variableId = "xxx";
-var topic = 'rt/variables/'+variableId+'/last_value';
-redisClient.publish(topic, '{"value": 20,"timestamp": 1000, "context": {"lat":1.1, "lng":10.01}, "id": "77736663dhhhdh"}');
